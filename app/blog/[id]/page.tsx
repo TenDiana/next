7bb8f7c25f8d8a8348e244b1d83b717574e49645
TenDiana <dinkss2552@gmail.com>
2023-09-04 00:24:43 +0600

    mini blog & try async fetch

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+import React from "react";
+// import {fetch} from "next/dist/compiled/@edge-runtime/primitives";
+async function getData(id: string) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        next: {
+            revalidate: 60
+        }
+    });
+
+    return response.json();
+}
+type Props = {
+    params: {
+        id: string //Именно название папки!
+    }
+}
+
+export default async function Post({params: {id}}: Props) {
+    const post = await getData(id);
+    return (
+        <>
+            <h1>{post.title}</h1>
+            <h3>{post.body}</h3>
+        </>
+    )
+}
